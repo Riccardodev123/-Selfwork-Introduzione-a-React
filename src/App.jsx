60decf9ded34a343e864b5082807256291256673
Riccardo Giordanella <it.gg@hotmail.it>
2025-10-09 22:25:23 +0200

    End selfwork

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="title">First React Project</h1>
+      <label htmlFor="input" className="label">Test</label>
+      <input type="text" id="input" className="input"/>
+    </>
+  );
+}
+
+export default App;
